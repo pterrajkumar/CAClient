2bebf671cc3ab886ab6a79c840bf7885961e2257
Rajkumar Peter <rpeter001@nam.ad.pwcinternal.com>
2018-03-15 23:54:49 +0530

    Infinite Scroll fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LazyLoadImagesModule } from 'ngx-lazy-load-images';
 import { FishesService } from '../service/fishes-service.service';
+import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
 
 @Component({
   selector: 'app-home',
@@ -11,22 +12,34 @@ export class HomeComponent implements OnInit {
   currentIndex = 1;
   fishes: Array<any> = [];
   errorMessage: string;
-  
+  indexArray: Array<string> = [];
   constructor(private fishesService: FishesService) { 
     this.onScroll();
   }
   
   ngOnInit() {
-
+    //this.currentIndex = 0;
   }
 
-  onScroll(){
+  onScroll(){    
+
     this.fishesService.getFishes(this.currentIndex)
     .subscribe(
       aquariumFishes => {
         this.currentIndex++;
-        console.log(aquariumFishes.json());
-        this.fishes = this.fishes.concat(aquariumFishes.json());
+        if(aquariumFishes.json().length > 0){
+        const index = aquariumFishes.json()[0].index;
+        if(this.fishes.length == 0) {
+          this.indexArray.push(index);
+          this.fishes = this.fishes.concat(aquariumFishes.json());
+        }else{
+          const bIndex: number = this.indexArray.indexOf(index);
+          if (bIndex == -1) {
+            this.indexArray.push(index);
+            this.fishes = this.fishes.concat(aquariumFishes.json());
+          }          
+        } 
+      }       
       },
       (error: any) => this.errorMessage = <any>error  
     );
