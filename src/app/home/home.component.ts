6ef6134c4c0445b118af154f6e8f7a0d16209266
Rajkumar Peter <rpeter001@nam.ad.pwcinternal.com>
2018-03-16 19:25:41 +0530

    UI Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,42 +6,60 @@ import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   currentIndex = 1;
+  totalFishes = 1;
   fishes: Array<any> = [];
   errorMessage: string;
   indexArray: Array<string> = [];
   constructor(private fishesService: FishesService) { 
-    this.onScroll();
+    this.getTotalFishes();    
   }
   
   ngOnInit() {
-    //this.currentIndex = 0;
+    this.onScroll();
   }
 
   onScroll(){    
-
-    this.fishesService.getFishes(this.currentIndex)
-    .subscribe(
-      aquariumFishes => {
+    if(this.currentIndex <= this.totalFishes){      
+      
+      const tempIndex:string = String(this.currentIndex);
+      const chkIndex: number = this.indexArray.indexOf(tempIndex);
+      if(chkIndex != -1){
         this.currentIndex++;
-        if(aquariumFishes.json().length > 0){
-        const index = aquariumFishes.json()[0].index;
-        if(this.fishes.length == 0) {
-          this.indexArray.push(index);
-          this.fishes = this.fishes.concat(aquariumFishes.json());
-        }else{
-          const bIndex: number = this.indexArray.indexOf(index);
-          if (bIndex == -1) {
-            this.indexArray.push(index);
-            this.fishes = this.fishes.concat(aquariumFishes.json());
-          }          
-        } 
-      }       
+      }
+
+      this.fishesService.getFishes(this.currentIndex)
+      .subscribe(
+        aquariumFishes => {          
+          if(aquariumFishes.json().length > 0){
+            const index = aquariumFishes.json()[0].index;
+            if(this.fishes.length == 0) {
+              this.indexArray.push(index);
+              this.fishes = this.fishes.concat(aquariumFishes.json());
+            }else{
+              const bIndex: number = this.indexArray.indexOf(index);
+              if (bIndex == -1) {
+                this.indexArray.push(index);
+                this.fishes = this.fishes.concat(aquariumFishes.json());
+              }          
+            }            
+          }       
+        },
+        (error: any) => this.errorMessage = <any>error  
+      );
+    }
+  }
+
+  getTotalFishes(){
+    this.fishesService.getTotalFishes()
+    .subscribe(
+      fishes => {
+        this.totalFishes = parseInt(JSON.stringify(fishes.json()), 10);        
       },
-      (error: any) => this.errorMessage = <any>error  
+      (error: any) => this.errorMessage = <any>error
     );
   }
 }
--- src/app/service/fishes-service.service.ts
+++ src/app/service/fishes-service.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-const BASE_URL = 'http://rajkumarpeter.southcentralus.cloudapp.azure.com:8080';
-//const BASE_URL = 'http://localhost:3000';
+//const BASE_URL = 'http://rajkumarpeter.southcentralus.cloudapp.azure.com:8080';
+const BASE_URL = 'http://localhost:3000';
 
 @Injectable()
 export class FishesService {
 
   constructor(private http: Http) { }
+
+  getTotalFishes(){
+    return this.http.get(`${BASE_URL}/api/totalFishes`);
+  }
 
   getFishes(currentIndex: number) {
     return this.http.get(`${BASE_URL}/api/fishes/${currentIndex}`);
